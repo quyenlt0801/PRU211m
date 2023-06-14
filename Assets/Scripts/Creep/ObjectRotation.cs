505598b871b80d5260f8789a7220397a954855ac
taidmhe150076 <107052401+taidmhe150076@users.noreply.github.com>
2023-06-15 00:11:38 +0700

    add prefabs Creep and ObjectRatation.cs( Creep xoay tròn quanh 1 điểm )
    add prefabs Creep and ObjectRatation.cs( Creep xoay tròn quanh 1 điểm )

---
+++ Assets/Scripts/Creep/ObjectRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    public Transform anchorPoint; // Điểm neo
+    public float rotationSpeed = 100f; // Tốc độ xoay
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tăng góc quay dựa trên tốc độ xoay và thời gian
+        float rotationAngle = rotationSpeed * Time.deltaTime;
+
+        // Sử dụng hàm transform.Rotate để xoay đối tượng
+        transform.RotateAround(anchorPoint.position, Vector3.forward, rotationAngle);
+    }
+}
