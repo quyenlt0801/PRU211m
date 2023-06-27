35c8b724610c8700e04a2026983ac60166ec377d
taidmhe150076 <107052401+taidmhe150076@users.noreply.github.com>
2023-06-27 11:26:46 +0700

    add script Bullet_Player  and Prefabs Player
    add script Bullet_Player  and Prefabs Player

---
+++ Assets/Scripts/Player/Bullet_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    private void Update()
+    {
+        float moveDistance = moveSpeed * Time.deltaTime;
+        transform.Translate(Vector2.right * moveDistance);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Kiểm tra nếu vật thể va chạm là vật thể khác
+        if (collision.gameObject.CompareTag("Creeps"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+
     private Rigidbody2D rb;
     private float moveSpeed;
+
+    public float shootCooldown = 1f; // Thời gian chờ giữa các lần bắn
+    private float shootTimer = 0f; // Thời gian đã trôi qua từ lần bắn trước đó
+    private bool canShoot = true; // Cho phép bắn hay không
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         moveSpeed = 5f;
     }
+    private void Update()
+    {
+        if (!canShoot)
+        {
+            shootTimer += Time.deltaTime;
+            if (shootTimer >= shootCooldown)
+            {
+                canShoot = true;
+            }
+        }
+    }
 
-    
     public void MoveUp()
     {
         rb.velocity = Vector2.up * moveSpeed;
@@ -27,5 +44,14 @@ public class PlayerController : MonoBehaviour
     public void StopMoving()
     {
         rb.velocity = Vector2.zero;
+    }
+    public void Shoot()
+    {
+        if (canShoot)
+        {
+            Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+            shootTimer = 0f;
+            canShoot = false;
+        }
     }
 }
