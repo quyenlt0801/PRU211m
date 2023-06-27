5cd3dc7f3347d196b969d7c360ea17ca1b1132fc
taidmhe150076 <quyenlt08012001@gmail.com>
2023-06-27 17:41:41 +0700

    ADD menu and script MENU
    ADD menu and script MENU

---
+++ Assets/Scripts/Map/MENU_Manager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
